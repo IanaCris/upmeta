59d95e097f4c4e3b8ff880aa7f1fde4fdba0443b
IanaCris <ianasousati@gmail.com>
2022-09-07 21:29:18 -0300

    Project created

---
+++ backend/src/main/java/com/ianadev/upmeta/UpmetaApplication.java
@@ -0,0 +1,13 @@
+package com.ianadev.upmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ianadev/upmeta/UpmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianadev.upmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
